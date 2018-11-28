ee6b6f7a9ea8158a6d23f9b82649c624ad345022
Enea Dodi <dodi@chapman.edu>
2018-11-28 05:29:59 +0000

    first commit

--- test_sinesum.py
+++ test_sinesum.py
@@ -7,6 +7,13 @@ Verify implementation of the Fourier sine series using numpy arrays.
 
 import sinesum
 
-def test_dummy():
-    """Dummy test that always fails. Please replace with actual tests."""
-    assert False, "This is a dummy test"
+
+def test_f():
+ """Test function to check if f function in cw08 module works correctly"""
+ assert sinesum.f(0) == 0
+    assert sinesum.f(0.3*math.pi*2,100) == 1
+    assert math.isclose(sinesum.f(-1),1)
+
+def test_Sn():
+    """Test Function to check if Sn functino in cw08 module works correctly"""
+    assert sinesum.Sn(1.5,100)
