9cd03c3d757bb485e10f3d40d588c2a72099eeb2
eneadodi <41304927+eneadodi@users.noreply.github.com>
2018-12-04 18:15:27 -0800

    Update test_sinesum.py

--- test_sinesum.py
+++ test_sinesum.py
@@ -9,11 +9,11 @@ import sinesum
 
 
 def test_f():
- """Test function to check if f function in cw08 module works correctly"""
+    """Test function to check if f function in cw08 module works correctly"""
     assert sinesum.f(0) == 0
     assert sinesum.f(0.3*math.pi*2,100) == 1
     assert math.isclose(sinesum.f(-1),1)
 
 def test_Sn():
     """Test Function to check if Sn functino in cw08 module works correctly"""
-    assert sinesum.Sn(1.5,100)
+    assert math.isclose(sinesum.Sn(math.pi*2*0.25,100),1)
